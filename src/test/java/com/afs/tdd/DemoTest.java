5b89105d5cd8996fa893f4eed30b01469ae0862e
BORBECH <charlieborbs18@gmail.com>
2020-10-15 18:34:20 +0800

    [MarsRover][Charlie] test : Create Test that execute command M with given location and heading (0,0,S)

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -51,6 +51,21 @@ class DemoTest {
     }
 
     @Test
+    void should_x_0_y_1_heading_S_when_execute_given_x_0_y_0_heading_S_command_M() {
+        //given
+        MarsRover marsRover = new MarsRover(0, 0, "S");
+
+        //when
+        String command = "M";
+        marsRover.executeCommands(command);
+
+        //then
+        assertEquals(0, marsRover.getLocationX());
+        assertEquals(-1, marsRover.getLocationY());
+        assertEquals("S", marsRover.getHeading());
+    }
+
+    @Test
     void should_x_0_y_0_heading_N_return_x_1_y_0_heading_N_given_MLMR() {
         //given
         MarsRover marsRover = new MarsRover(0, 0, "N");
